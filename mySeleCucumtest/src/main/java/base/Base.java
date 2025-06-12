df56854b67286ca9bc1d5396e2b5d249056dbf7b
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-12 21:17:00 +0530

    Update Base.java

--- mySeleCucumtest/src/main/java/base/Base.java
+++ mySeleCucumtest/src/main/java/base/Base.java
@@ -5,17 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
-    public static WebDriver Driver;
+    public static WebDriver driver;
 
     public void setUp() {
-        WebDriverManager.chromedriver().setup(); // Dynamically downloads chromedriver
-        Driver = new ChromeDriver(); // <- Make sure it's assigned to Driver, not 'driver'
-        Driver.get("file:///D:/DevOps/Projects/mySeleCucumtest/testpage/login.html");
-    }
+    WebDriverManager.chromedriver().setup();
+    Driver = new ChromeDriver();
+    Driver.manage().window().maximize();  //  optional but recommended
+    Driver.get("file:///D:/DevOps/Projects/mySeleCucumtest/testpage/login.html");
+}
 
     public void tearDown() {
         if (Driver != null) {
-            Driver.quit();
+            Driver.quit(); //  properly closes the browser
         }
     }
 }
