bac59b3fa566986842715d8137c801d5119fe656
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-12 21:17:57 +0530

    Update Hook.java

--- mySeleCucumtest/src/test/java/steps/Hook.java
+++ mySeleCucumtest/src/test/java/steps/Hook.java
@@ -3,12 +3,11 @@ package steps;
 import base.Base;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 
 public class Hook extends Base {
+
     @Before
-    public void InitializeTest(Scenario scenario) {
-        System.out.println("Initializing Test: " + scenario.getName());
+    public void InitializeTest() {
         setUp();
     }
 
