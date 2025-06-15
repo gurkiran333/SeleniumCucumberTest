403a9d33307f6c8d74f20460ac62a61dcb699231
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-15 19:53:02 +0530

    Update Hook.java

--- mySeleCucumtest/src/test/java/steps/Hook.java
+++ mySeleCucumtest/src/test/java/steps/Hook.java
@@ -4,14 +4,20 @@ import io.cucumber.java.Before;
 import io.cucumber.java.After;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     public static WebDriver driver;
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\ChromeDriver137\\chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        // Optional: Uncomment if you're running tests in Jenkins or headless environment
+        // options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
