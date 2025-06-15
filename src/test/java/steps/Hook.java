28fd6893e07ae9fcbc715ffe2c628787bcb2cac2
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-15 21:59:11 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -2,29 +2,27 @@ package steps;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
-import org.openqa.selenium.WebDriver;
+import base.Base;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
-    public static WebDriver driver;
 
     @Before
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        // Optional: Uncomment if you're running tests in Jenkins or headless environment
         // options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+        Base.driver = new ChromeDriver(options); 
+        Base.driver.manage().window().maximize();
     }
 
     @After
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+        if (Base.driver != null) {
+            Base.driver.quit();  
         }
     }
 }
