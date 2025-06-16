fa0e35945da953c36531e2a3589a6893c27894af
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-16 13:33:44 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -1,28 +1,18 @@
 package steps;
 
+import base.Base;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
-import base.Base;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Hook {
+public class Hook extends Base {
 
     @Before
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        Base.driver = new ChromeDriver(options); 
-        Base.driver.manage().window().maximize();
+    public void initializeDriver() {
+        setUp(); // uses setup method from Base.java
     }
 
     @After
-    public void tearDown() {
-        if (Base.driver != null) {
-            Base.driver.quit();  
-        }
+    public void closeDriver() {
+        tearDown(); // uses teardown method from Base.java
     }
 }
