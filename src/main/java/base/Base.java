85f1db1a9bac3785cdf75d3574807b045e61d7ef
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-16 13:37:40 +0530

    Update Base.java

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,12 +1,29 @@
-public void setUp() {
-    WebDriverManager.chromedriver().setup();
+package base;
 
-    ChromeOptions options = new ChromeOptions();
-    // options.addArguments("--headless=new");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-    driver = new ChromeDriver(options);
-    driver.manage().window().maximize();
-    driver.get("file:///D://DevOps//Projects//mySeleCucumtest//testpage//login.html");
+public class Base {
+    public static WebDriver driver;
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--headless=new"); // enable in CI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);  
+        driver.manage().window().maximize(); 
+        driver.get("file:///D://DevOps//Projects//mySeleCucumtest//testpage//login.html");
+    }
+
+    public void tearDown() {
+        if (driver != null) {   
+            driver.quit();
+        }
+    }
 }
