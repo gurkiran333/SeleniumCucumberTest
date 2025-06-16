2680db3f064bee323b76ab92e52b46525bdad901
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-16 13:41:54 +0530

    Update LoginStep.java

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -4,7 +4,6 @@ import base.Base;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,7 +14,7 @@ public class LoginStep extends Base {
     @Then("I should see the userform page")
     public void iShouldSeeTheUserformPage() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Initial")));
-        Assert.assertTrue("User form is not visible", element.isDisplayed());
+        boolean isDisplayed = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Initial"))).isDisplayed();
+        Assert.assertTrue("Userform page not displayed", isDisplayed);
     }
 }
