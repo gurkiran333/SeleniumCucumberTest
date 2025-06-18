67c25f10f9f7690d8e1f9eed4cf59fc45aeeb02d
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-18 15:09:25 +0530

    Update Base.java

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,12 +1,16 @@
-public class Base {
-    public static WebDriver driver; // make it static
+package base;
 
-    public WebDriver setUp() {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+    public static WebDriver driver;
+
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "D://DevOps//Projects//SeleniumCucumberTest//path//to//chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("file:///D://DevOps//Projects//mySeleCucumtest//testpage//login.html");
-        return driver;
     }
 
     public void tearDown() {
