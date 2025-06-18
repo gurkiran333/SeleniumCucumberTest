35dd057fba0048259b08fa2b5ad4b633b64b056d
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-18 14:43:31 +0530

    Update Base.java

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -2,27 +2,21 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
-    public static WebDriver driver;
 
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
+    protected WebDriver driver;
 
-        ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--headless=new"); // enable in CI
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        driver = new ChromeDriver(options);  
-        driver.manage().window().maximize(); 
+    public WebDriver setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("file:///D://DevOps//Projects//mySeleCucumtest//testpage//login.html");
+        return driver;
     }
 
     public void tearDown() {
-        if (driver != null) {   
+        if (driver != null) {
             driver.quit();
         }
     }
