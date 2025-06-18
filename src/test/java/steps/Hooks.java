060ffe98992af3afa59dacf8f8b81fb5f4c1cf14
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-18 14:55:24 +0530

    Update Hooks.java

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,22 +1,18 @@
-// File: Hooks.java
 package hooks;
 
 import base.Base;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
-import org.openqa.selenium.WebDriver;
 
 public class Hooks extends Base {
 
-    public static WebDriver driver;
-
     @Before
     public void initializeDriver() {
-        driver = setUp();
+        setUp();  // initializes static driver
     }
 
     @After
     public void closeDriver() {
-        tearDown();
+        tearDown();  // cleans up driver
     }
 }
