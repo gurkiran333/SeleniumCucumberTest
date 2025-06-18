3fe0d6701e678ab5a30d88cb95ed550b2a15ef69
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-18 15:05:45 +0530

    Update Base.java

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,23 +1,11 @@
-package base;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Base {
-
-    // Make this static if you want to access it from Hooks or Step classes without creating a new object
-    public static WebDriver driver;
+    public static WebDriver driver; // make it static
 
     public WebDriver setUp() {
-        //  Fix the path: remove "path/to/" and use actual path to ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "D:\\DevOps\\Projects\\SeleniumCucumberTest\\drivers\\chromedriver.exe");
-
+        System.setProperty("webdriver.chrome.driver", "D://DevOps//Projects//SeleniumCucumberTest//path//to//chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
-        //  Make sure the HTML file path is correct
-        driver.get("file:///D:/DevOps/Projects/mySeleCucumtest/testpage/login.html");
-
+        driver.get("file:///D://DevOps//Projects//mySeleCucumtest//testpage//login.html");
         return driver;
     }
 
