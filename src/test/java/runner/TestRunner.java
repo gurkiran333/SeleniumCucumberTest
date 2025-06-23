ed3350ea0c700390a3e59770f093f91aa4d4ed99
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-23 13:27:29 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
-    glue = "steps",
+    glue = {"steps", "hooks", "base"},  // include all packages with step defs and hooks
     plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"}
 )
 public class TestRunner {}
