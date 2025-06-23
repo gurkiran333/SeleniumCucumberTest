ff307717109c1885be08f4265bd1df40a18d756f
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2025-06-23 13:28:41 +0530

    Update Hooks.java

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -4,15 +4,16 @@ import base.Base;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
 
-public class Hooks extends Base {
+public class Hooks {
+    Base base = new Base();
 
     @Before
-    public void initializeDriver() {
-        setUp(); // initializes the static driver
+    public void beforeScenario() {
+        base.setUp();  // initializes driver
     }
 
     @After
-    public void closeDriver() {
-        tearDown(); // quits driver after scenario
+    public void afterScenario() {
+        base.tearDown();  // closes browser
     }
 }
